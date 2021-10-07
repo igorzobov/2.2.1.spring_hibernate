7fcada06a24daca7395d3269330aaea1d6cc7ab7
igorzobov <igor_zobov@mail.ru>
2021-10-07 15:05:12 +0700

    done

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -1,10 +1,12 @@
 package hiber.dao;
 
+import hiber.model.Car;
 import hiber.model.User;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
@@ -24,6 +26,13 @@ public class UserDaoImp implements UserDao {
    public List<User> listUsers() {
       TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
       return query.getResultList();
+   }
+
+   @Override
+   public User get(Car car) {
+      Query query = sessionFactory.getCurrentSession().createQuery("from User where car =:car_id");
+      query.setParameter("car_id",car);
+      return (User)query.getSingleResult();
    }
 
 }
--- src/main/java/hiber/service/UserService.java
+++ src/main/java/hiber/service/UserService.java
@@ -1,10 +1,12 @@
 package hiber.service;
 
+import hiber.model.Car;
 import hiber.model.User;
 
 import java.util.List;
 
 public interface UserService {
+    User get(Car car);
     void add(User user);
     List<User> listUsers();
 }
